bd1e24ee3c9d24a72185f1a15a81b6c30707a474
Ernesto Rubio F <neto.r27@gmail.com>
2012-07-30 22:49:11 -0500

    Modulo Pagos
    Se elimino la accion receptorMensajeIpnPayPal de pagosControlador. Ya que esta acción se realiza otro lugar

--- modulos/pagos/controladores/pagosControlador.php
+++ modulos/pagos/controladores/pagosControlador.php
@@ -7,8 +7,3 @@ function principal() {
 function pagoSatisfactorio() {    
     require_once 'modulos/pagos/vistas/pagoSatisfactorio.php';    
 }
-
-function receptorMensajeIpnPayPal(){
-    require_once 'modulos/pagos/clases/manejadorPagosPayPal.php';
-    handlePaypalIpnMessage();
-}
