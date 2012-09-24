2c1618d9b878b34279b215c905719b98147a105b
Ernesto Rubio F <neto.r27@gmail.com>
2012-09-23 22:18:25 -0500

    Test
    Test de eval en test.php

--- test.php
+++ test.php
@@ -1,17 +1,27 @@
 <?php
 
-require_once('modulos/pagos/modelos/ipnModelo.php');
+//require_once('modulos/pagos/modelos/ipnModelo.php');
+//
+//$ipnMensaje = new IpnMensaje();
+//
+//if (txnRecibido($ipnMensaje->txn_id)) {
+//    echo "El txn_id=" . $ipnMensaje->txn_id . " ya había sido recibido";
+//} else {
+//    $id = agregarIpnMensaje($ipnMensaje);
+//    if ($id >= 0) {
+//        echo "Se agregó correctamente el mensaje\n\n\n";
+//    }else{
+//        echo "error al agregar a la bd";
+//    }
+//}
+//
 
-$ipnMensaje = new IpnMensaje();
+$total = 100;
+$cadena = "($total * 0.05) + 2";
 
-if (txnRecibido($ipnMensaje->txn_id)) {
-    echo "El txn_id=" . $ipnMensaje->txn_id . " ya había sido recibido";
-} else {
-    $id = agregarIpnMensaje($ipnMensaje);
-    if ($id >= 0) {
-        echo "Se agregó correctamente el mensaje\n\n\n";
-    }else{
-        echo "error al agregar a la bd";
-    }
-}
+$cargoExtra = 0;
+eval('$cargoExtra = ' . $cadena . ';');
+
+//aqui cargoextra ya tiene el valor del gasto de envi
+echo $cargoExtra;
 ?>
