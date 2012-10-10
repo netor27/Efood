9c9923676e6f05ad4114d4091ecdbc7c5a10c9f8
Ernesto Rubio F <neto.r27@gmail.com>
2012-10-10 11:32:52 -0500

    Twitter Bootstrap
    Se agrego la librería de twitter bootstrap y se pusieron los css y js necesarios en headInicio.php

--- layout/headers/headInicio.php
+++ layout/headers/headInicio.php
@@ -7,5 +7,10 @@
         <title>efood - A un click de tu comida</title>
         <script src="js/jquery-1.7.1.min.js"></script>	
         <script src="js/jquery-ui-1.8.18.custom.min.js"></script>	
+        <script src="/lib/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/lib/bootstrap/js/bootbox.min.js"></script>
         <link rel="stylesheet" href="layout/css/sunny/jquery-ui-1.8.18.custom.css" />
         <link rel="stylesheet" href="layout/css/MainStyle.css" />
+        <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="/lib/bootstrap/css/bootstrap.min.css">
+        
