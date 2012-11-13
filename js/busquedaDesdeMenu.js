280f7780fa172d6796e80737c913c55b8150a7bd
Ernesto Rubio <neto.r27@gmail.com>
2012-11-12 22:01:10 -0600

    Bug con el cuadero de restaurante del menú

---
+++ js/busquedaDesdeMenu.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){	
+    $('select.selectBlanco').each(function(){
+        var title = $(this).attr('title');
+        if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
+        $(this)
+        .css({
+            'z-index':10,
+            'opacity':0,
+            '-khtml-appearance':'none'
+        })
+        .after('<span class="selectBlanco">' + title + '</span>')
+        .change(function(){                        
+            val = $('option:selected',this).text();
+            if(val != ''){
+                $(this).next().text(val);
+            }else{
+                var title= $(this).attr('title');
+                $(this).next().text(title);
+            }
+        })
+    });    
+    $("#colonias").click(function(){
+        $(this).val("");
+    });
+});
--- js/resultadosBusqueda.js
+++ js/resultadosBusqueda.js
@@ -16,27 +16,6 @@ $(document).ready(function(){
         $("#modalDialogNoHayRestaurantes").dialog("close");
     });
     
-    $('select.selectBlanco').each(function(){
-        var title = $(this).attr('title');
-        if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
-        $(this)
-        .css({
-            'z-index':10,
-            'opacity':0,
-            '-khtml-appearance':'none'
-        })
-        .after('<span class="selectBlanco">' + title + '</span>')
-        .change(function(){                        
-            val = $('option:selected',this).text();
-            if(val != ''){
-                $(this).next().text(val);
-            }else{
-                var title= $(this).attr('title');
-                $(this).next().text(title);
-            }
-        })
-    });
-    
     $('select.selectAmarillo').each(function(){
         var title = $(this).attr('title');
         if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
